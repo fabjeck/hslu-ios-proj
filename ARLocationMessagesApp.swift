e4454ca6fcce7b60f42afc4097d5cc961974bad8
Fabien Jeckelmann <fabien.jeckelmann@gmail.com>
2020-12-07 15:49:24 +0100

    project restructure

---
+++ ARLocationMessagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARLocationMessagesApp.swift
+//  ARLocationMessages
+//
+//  Created by Fabien Jeckelmann on 07.12.20.
+//
+
+import SwiftUI
+
+@main
+struct ARLocationMessagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
