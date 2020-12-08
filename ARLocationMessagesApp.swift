822fe3795777bf1b77719393785c8230475e4ff0
Merc <mc.hausler@hotmail.com>
2020-12-08 17:27:08 +0100

    added coredata
    no usage yet

--- ARLocationMessagesApp.swift
+++ ARLocationMessagesApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 @main
 struct ARLocationMessagesApp: App {
@@ -26,3 +27,35 @@ struct ARLocationMessagesApp: App {
         }
     }
 }
+var persistentContainer: NSPersistentContainer = {
+  // 2
+  let container = NSPersistentContainer(name: "MessagesModel")
+  // 3
+  container.loadPersistentStores { _, error in
+    // 4
+    if let error = error as NSError? {
+      // You should add your own error handling code here.
+      fatalError("Unresolved error \(error), \(error.userInfo)")
+    }
+  }
+  return container
+}()
+
+func saveContext() {
+  // 1
+  let context = persistentContainer.viewContext
+  // 2
+  if context.hasChanges {
+    do {
+      // 3
+      try context.save()
+    } catch {
+      // 4
+      // The context couldn't be saved.
+      // You should add your own error handling here.
+      let nserror = error as NSError
+      fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+    }
+  }
+}
+
